a3653f13f631b6e627976e3ddfdbe961db7bb8be
Rokesh <Rokesh@gamil.com>
2023-09-13 12:26:53 +0530

    second commit

--- Rokesh.java
+++ Rokesh.java
@@ -1,7 +1,11 @@
-class Rokesh
+import java.io.*;
+
+public class Rokesh
 {
 public static void main(String[] args)
 {
+System.out.println("Hello,world from GIT!");
+System.out.println("Hello,world from GIT!");
 System.out.println("Hello,world from GIT!");
 }
 }
